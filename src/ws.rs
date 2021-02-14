db85dcc3683d3680da014ccc3d94108ee8c79abf
Magomed Arsaev <arsaev.m@gmail.com>
2021-02-14 13:49:32 +0100

    Add redis channel subscription
    Code is more a PoC and will be improved in next commits.

--- src/ws.rs
+++ src/ws.rs
@@ -1,17 +1,66 @@
+extern crate redis;
 use crate::{Client, Clients};
 use futures::{FutureExt, StreamExt};
 use serde::Deserialize;
-use serde_json::from_str;
+// use serde_json::from_str;
 use tokio::sync::mpsc;
 use warp::ws::{Message, WebSocket};
+use redis::{Client as RClient, ControlFlow, PubSubCommands};
+use std::sync::Arc;
+use std::thread;
+// use std::time::Duration;
 
 #[derive(Deserialize, Debug)]
 pub struct TopicsRequest {
     topics: Vec<String>,
 }
 
-pub async fn client_connection(ws: WebSocket, id: String, clients: Clients, mut client: Client) {
-    let (client_ws_sender, mut client_ws_rcv) = ws.split();
+trait AppState {
+    fn client(&self) -> &Arc<RClient>;
+}
+
+struct Ctx {
+    pub client: Arc<RClient>,
+}
+
+impl Ctx {
+    fn new() -> Ctx {
+        let client = RClient::open("redis://localhost:6379").unwrap();
+        Ctx {
+            client: Arc::new(client),
+        }
+    }
+}
+
+impl AppState for Ctx {
+    fn client(&self) -> &Arc<RClient> {
+        &self.client
+    }
+}
+//
+// fn subscribe(state: &impl AppState) -> thread::JoinHandle<()> {
+//     let client = Arc::clone(state.client());
+//     thread::spawn(move || {
+//         let mut conn = client.get_connection().unwrap();
+//
+//         conn.subscribe(&["boo"], |msg| {
+//             let ch = msg.get_channel_name();
+//             let payload: String = msg.get_payload().unwrap();
+//             match payload.as_ref() {
+//                 "10" => ControlFlow::Break(()),
+//                 a => {
+//                     println!("Channel '{}' received '{}'.", ch, a);
+//                     ControlFlow::Continue
+//                 }
+//             }
+//         }).unwrap();
+//     })
+// }
+
+
+
+pub async fn client_connection(ws: WebSocket, id: String, clients: Clients, client: Client) {
+    let (client_ws_sender, mut _client_ws_rcv) = ws.split();
     let (client_sender, client_rcv) = mpsc::unbounded_channel();
 
     tokio::task::spawn(client_rcv.forward(client_ws_sender).map(|result| {
@@ -20,47 +69,28 @@ pub async fn client_connection(ws: WebSocket, id: String, clients: Clients, mut 
         }
     }));
 
-    client.sender = Some(client_sender);
+    // client.sender = Some(client_sender);
     clients.write().await.insert(id.clone(), client);
-
     println!("{} connected", id);
+    let ctx = Ctx::new();
+    let rclient = Arc::clone(ctx.client());
+    let mut conn = rclient.get_connection().unwrap();
+    thread::spawn(move || {
+        conn.subscribe(&["boo"], |msg| {
+            let ch = msg.get_channel_name();
+            let payload: String = msg.get_payload().unwrap();
+            let message_text = format!("Channel '{}' received '{}'.", ch, payload);
 
-    while let Some(result) = client_ws_rcv.next().await {
-        let msg = match result {
-            Ok(msg) => msg,
-            Err(e) => {
-                eprintln!("error receiving ws message for id: {}): {}", id.clone(), e);
-                break;
+            let _ = client_sender.send(Ok(Message::text(message_text.clone())));
+            println!("Channel '{}' received '{}'", ch, payload);
+
+            match payload.as_ref() {
+                "10" => ControlFlow::Break(()),
+                _a => {
+                    ControlFlow::Continue
+                }
             }
-        };
-        client_msg(&id, msg, &clients).await;
-    }
-
-    clients.write().await.remove(&id);
-    println!("{} disconnected", id);
-}
-
-async fn client_msg(id: &str, msg: Message, clients: &Clients) {
-    println!("received message from {}: {:?}", id, msg);
-    let message = match msg.to_str() {
-        Ok(v) => v,
-        Err(_) => return,
-    };
-
-    if message == "ping" || message == "ping\n" {
-        return;
-    }
-
-    let topics_req: TopicsRequest = match from_str(&message) {
-        Ok(v) => v,
-        Err(e) => {
-            eprintln!("error while parsing message to topics request: {}", e);
-            return;
-        }
-    };
-
-    let mut locked = clients.write().await;
-    if let Some(v) = locked.get_mut(id) {
-        v.topics = topics_req.topics;
-    }
+        }).unwrap();
+    });
+    // println!("{} disconnected", id);
 }
